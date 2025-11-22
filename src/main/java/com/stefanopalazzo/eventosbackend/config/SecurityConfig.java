3013cce26632c6fd56d98922b42a1ab0f3a57a83
Stefano Palazzo <spalazzo@seenka.com>
2025-11-22 20:30:03 -0300

    Implementa endpoint de login con seguridad JWT

---
+++ src/main/java/com/stefanopalazzo/eventosbackend/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.stefanopalazzo.eventosbackend.config;
+
+import com.stefanopalazzo.eventosbackend.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
--- src/test/java/com/stefanopalazzo/eventosbackend/EventosBackendApplicationTests.java
+++ src/test/java/com/stefanopalazzo/eventosbackend/EventosBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stefanopalazzo.eventosbackend.eventosbackend;
+package com.stefanopalazzo.eventosbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
