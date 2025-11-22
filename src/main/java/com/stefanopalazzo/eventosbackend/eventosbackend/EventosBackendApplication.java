b2753cf14985ebfafa265948ae93696d04285772
Stefano Palazzo <spalazzo@seenka.com>
2025-11-22 19:56:22 -0300

    Proyecto Spring Boot Inicial

---
+++ src/main/java/com/stefanopalazzo/eventosbackend/eventosbackend/EventosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stefanopalazzo.eventosbackend.eventosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventosBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stefanopalazzo/eventosbackend/eventosbackend/EventosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanopalazzo.eventosbackend.eventosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
